4d4e18e1700ac60b458ba9d8eb29880b941d5172
unknown <omar.rumbo33@gmail.com>
2022-04-02 13:12:44 +0200

    Query completa
    Falta elegir el csv por parametro

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pandasql as ps
+
+df = pd.read_csv('../examples/yellow_tripdata_2021-01.csv')
+zones = pd.read_csv('zones/taxi+_zone_lookup.csv')
+
+distance = df.trip_distance.quantile(0.95)
+
+q="""SELECT count(1) as trips, zones.Borough as end_borough, zones.Zone as end_zone 
+    from df 
+    inner join zones 
+        on df.PULocationID = zones.LocationID 
+    where trip_distance >{0}
+    group by df.PULocationID 
+    order by trips desc limit 10 """.format(distance)
+
+print (ps.sqldf(q,locals()))
