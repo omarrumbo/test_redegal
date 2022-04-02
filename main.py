084d43ba2bb8de52d33986467739658c4b6a11eb
unknown <omar.rumbo33@gmail.com>
2022-04-02 14:03:14 +0200

    Cambio localizacion origen por destino

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pandasql as ps
 import sys
 
-df = pd.read_csv('../examples/yellow_tripdata_2021-01.csv')
+df = pd.read_csv(sys.argv[1])
 zones = pd.read_csv('zones/taxi+_zone_lookup.csv')
 
 distance = df.trip_distance.quantile(0.95)
@@ -10,9 +10,9 @@ distance = df.trip_distance.quantile(0.95)
 q="""SELECT count(1) as trips, zones.Borough as end_borough, zones.Zone as end_zone 
     from df 
     inner join zones 
-        on df.PULocationID = zones.LocationID 
+        on df.DOLocationID = zones.LocationID 
     where trip_distance >{0}
-    group by df.PULocationID 
+    group by df.DOLocationID 
     order by trips desc limit 10 """.format(distance)
 
 print (ps.sqldf(q,locals()))
